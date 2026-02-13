710c12aea3cdf7247529a3ed81ef0f241c1de731
Michael Sims <michael.sims@trillianthealth.com>
2026-02-13 11:19:10 -0600

    Add pinned version of ktfmt 0.58

---
+++ ktfmt@0.58.rb
@@ -0,0 +1,33 @@
+class KtfmtAT058 < Formula
+  desc "Kotlin code formatter"
+  homepage "https://facebook.github.io/ktfmt/"
+  url "https://github.com/facebook/ktfmt/archive/refs/tags/v0.58.tar.gz"
+  sha256 "750c45507f0d1cb19bf687865876f1dc58e9527fe64d4c98b1d43b646bef72ec"
+  license "Apache-2.0"
+
+  depends_on "gradle" => :build
+  depends_on "openjdk@17"
+
+  def install
+    ENV["JAVA_HOME"] = Formula["openjdk@17"].opt_prefix
+
+    system "gradle", "shadowJar", "--no-daemon"
+    libexec.install "core/build/libs/ktfmt-#{version}-with-dependencies.jar"
+    bin.write_jar_script libexec/"ktfmt-#{version}-with-dependencies.jar", "ktfmt", java_version: "17"
+  end
+
+  test do
+    test_file = testpath/"Test.kt"
+    test_file.write <<~EOS
+      fun main() { println("Hello, World!") }
+    EOS
+
+    output = shell_output("#{bin}/ktfmt --google-style #{test_file} 2>&1")
+    assert_match "Done formatting #{test_file}", output
+    assert_equal <<~EOS, test_file.read
+      fun main() {
+        println("Hello, World!")
+      }
+    EOS
+  end
+end
